f1b6f86557aaae4a68269a62f17f1703d1ad551d
Marty <marty@thee.(none)>
2011-09-25 19:36:09 +1000

    Batch file for editing parameters

---
+++ join-example.cmd
@@ -0,0 +1,15 @@
+@echo off
+"%SystemDrive%\Python32\python.exe" join ^
+ca "%USERPROFILE%\My Documents\plant lists\Cmn plantlist\PLANT_ca.txt" ^
+freqs "%USERPROFILE%\My Documents\evcs\GoldfieldsBrgnlEVCSppFreq.xls.csv" ^
+grid 020 ^
+area AX7 ^
+evc "Grassy Woodland" ^
+evc "Creekline Grassy Woodland" ^
+evc "Granitic Hills Woodland" ^
+quad "%USERPROFILE%\My Documents\plant lists\Viridans Faraday NE.CSV" ^
+quad "%USERPROFILE%\My Documents\plant lists\Viridans Faraday NW.CSV" ^
+quad "%USERPROFILE%\My Documents\plant lists\Viridans Faraday SE.CSV" ^
+quad "%USERPROFILE%\My Documents\plant lists\Viridans Faraday SW.CSV" ^
+ > "join-output.html"
+pause
