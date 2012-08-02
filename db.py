a8dda8fd636253ddf2bbadf784b15b3b3fa003c6
Martin Panter <vadmium  gmailcom>
2012-08-02 21:39:24 +1000

    Fix indentation

--- db.py
+++ db.py
@@ -30,61 +30,61 @@ def CplExcelReader(file):
     
     with closing(excel_sheets(file)) as file:
         for sheet in file:
-                extra = dict()
-                expect_headings = False
-                for row in range(sheet.nrows):
-                    types = sheet.row_types(row)
-                    group_col = None
-                    for (col, type) in enumerate(types):
-                        if type in EXCEL_BLANKS:
-                            continue
-                        if group_col is None:
-                            group_col = col
-                        else:
-                            break
+            extra = dict()
+            expect_headings = False
+            for row in range(sheet.nrows):
+                types = sheet.row_types(row)
+                group_col = None
+                for (col, type) in enumerate(types):
+                    if type in EXCEL_BLANKS:
+                        continue
+                    if group_col is None:
+                        group_col = col
                     else:
-                        if group_col is not None:
-                            group = sheet.cell_value(row, group_col)
-                            extra.update(group=group.strip())
-                            expect_headings = True
-                        continue
-                    if expect_headings:
-                        fields = dict()
-                        for (col, heading) in enumerate(sheet.row_values(row)):
-                            try:
-                                field = HEADING_FIELDS[heading]
-                            except LookupError:
-                                continue
-                            fields[field] = col
-                        expect_headings = False
-                        continue
-                    
-                    fam_fields = parse_fields("""name, common""")
-                    for (col, type) in enumerate(types):
-                        if col in (fields[field] for field in fam_fields):
+                        break
+                else:
+                    if group_col is not None:
+                        group = sheet.cell_value(row, group_col)
+                        extra.update(group=group.strip())
+                        expect_headings = True
+                    continue
+                if expect_headings:
+                    fields = dict()
+                    for (col, heading) in enumerate(sheet.row_values(row)):
+                        try:
+                            field = HEADING_FIELDS[heading]
+                        except LookupError:
                             continue
-                        if type not in EXCEL_BLANKS:
-                            break
-                    else:
-                        family = Record()
-                        for name in fam_fields:
-                            value = sheet.cell_value(row, fields[name])
-                            setattr(family, name, value.strip())
-                        extra.update(family=family)
+                        fields[field] = col
+                    expect_headings = False
+                    continue
+                
+                fam_fields = parse_fields("""name, common""")
+                for (col, type) in enumerate(types):
+                    if col in (fields[field] for field in fam_fields):
                         continue
-                    
-                    plant = Record(extra)
-                    for (name, col) in fields.items():
-                        value = excel_value(sheet, row, col)
-                        if isinstance(value, Number):
-                            # Seen area=0 recorded as a number
-                            value = format(value, "g")
-                        elif value is not None:
-                            value = value.strip()
-                        setattr(plant, name, value)
-                    convert_empty(plant.__dict__,
-                        """vrots, weed, ex, area, note""")
-                    yield plant
+                    if type not in EXCEL_BLANKS:
+                        break
+                else:
+                    family = Record()
+                    for name in fam_fields:
+                        value = sheet.cell_value(row, fields[name])
+                        setattr(family, name, value.strip())
+                    extra.update(family=family)
+                    continue
+                
+                plant = Record(extra)
+                for (name, col) in fields.items():
+                    value = excel_value(sheet, row, col)
+                    if isinstance(value, Number):
+                        # Seen area=0 recorded as a number
+                        value = format(value, "g")
+                    elif value is not None:
+                        value = value.strip()
+                    setattr(plant, name, value)
+                convert_empty(plant.__dict__,
+                    """vrots, weed, ex, area, note""")
+                yield plant
 
 def FreqCsvReader(file):
     with open(file, newline="") as file:
